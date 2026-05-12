b049b59bd5e44c0a17cf8f0189f5744bb165584e
oruc <oruc.mamedov2005@gmail.com>
2026-05-12 20:06:25 +0400

    update

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
 const search = document.querySelector(".search");
 const productsContainer = document.querySelector(".products");
 
-const allProducts = [
-  {name:"Красные яблоки", category:"Фрукты", price:"1.90 ₼", img:"https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?auto=format&fit=crop&w=800&q=80"},
-  {name:"Бананы", category:"Фрукты", price:"2.20 ₼", img:"https://moya-planeta.ru/upload/images/xl/98/35/9835eed728bbae041b5f9a358ba78692720a331a.jpg"},
-  {name:"Апельсины", category:"Фрукты", price:"2.70 ₼", img:"https://www.greeninfo.ru/img/work/article/a_22251_54511.jpg"},
-  {name:"Киви", category:"Фрукты", price:"3.40 ₼", img:"https://img.gazeta.ru/files3/643/14262643/Depositphotos_137822298_XL-pic_32ratio_900x600-900x600-17051.jpg"},
-
-  {name:"Помидоры", category:"Овощи", price:"2.50 ₼", img:"https://images.techinsider.ru/upload/img_cache/3f6/3f67db05400a905e3e11259cfed8469d_cropped_510x340.webp"},
-  {name:"Огурцы", category:"Овощи", price:"1.80 ₼", img:"https://images.unsplash.com/photo-1449300079323-02e209d9d3a6?auto=format&fit=crop&w=800&q=80"},
-  {name:"Картофель", category:"Овощи", price:"1.20 ₼", img:"https://images.unsplash.com/photo-1518977676601-b53f82aba655?auto=format&fit=crop&w=800&q=80"},
-  {name:"Морковь", category:"Овощи", price:"1.40 ₼", img:"https://images.unsplash.com/photo-1447175008436-054170c2e979?auto=format&fit=crop&w=800&q=80"},
-
-  {name:"Молоко", category:"Молочные продукты", price:"1.80 ₼", img:"https://cdn.7days.ru/pic/1a2/974075/1395993/86.jpg"},
-  {name:"Сыр", category:"Молочные продукты", price:"4.50 ₼", img:"https://market-da.ru/assets/cache/images/news/2019/12/745x-shutterstock_390068599.b9b.jpg"},
-  {name:"Йогурт", category:"Молочные продукты", price:"2.10 ₼", img:"https://img.day.az/2017/10/11/940256_2.jpg"},
-  {name:"Масло", category:"Молочные продукты", price:"3.80 ₼", img:"https://baku.ws/storage/photos/uploads/thumbs/large/o0GVkkFMVxhvvcFXv6uCdhSXHj5LuVBMZYwHPL8t.webp"},
-
-  {name:"Говядина", category:"Мясо", price:"13.50 ₼", img:"https://shop.alem.ws/wa-data/public/photos/68/01/168/168.970.png"},
-  {name:"Курица", category:"Мясо", price:"8.20 ₼", img:"https://здоровое-питание.рф/upload/iblock/5ba/Depositphotos_48590785_S.jpg"},
-  {name:"Лосось", category:"Рыба", price:"14.90 ₼", img:"https://pionerprodukt.by/data/images/upl-20240116-496d6593bf.jpeg"},
-  {name:"Колбаса", category:"Мясо", price:"6.70 ₼", img:"https://assets.allcafe.ru/pic/566461154996.png"},
-
-  {name:"Свежий хлеб", category:"Выпечка", price:"0.70 ₼", img:"https://media.1777.ru/images/images_processing/835/8358568819666322.jpeg"},
-  {name:"Круассан", category:"Выпечка", price:"2.40 ₼", img:"https://lifehacker.ru/wp-content/uploads/2021/04/shutterstock_642373528_1619556387-scaled.jpg"},
-  {name:"Булочки", category:"Выпечка", price:"1.60 ₼", img:"https://img.iamcook.ru/2022/upl/recipes/cat/u-75d4aa109c11be63528a3cffe331ee2c.JPG"},
-  {name:"Пирог", category:"Выпечка", price:"4.80 ₼", img:"https://pirogoff.ee/wp-content/uploads/2022/05/%D0%BC%D0%B0%D0%BB%D0%B8%D0%BD%D0%B0-%D0%B4%D1%80%D0%BE%D0%B6%D0%B6%D0%B5%D0%B2%D0%BE%D0%B9-%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D0%BE%D0%B9-scaled.jpg"},
-
-  {name:"Газировка", category:"Напитки", price:"2.30 ₼", img:"https://images.unian.net/photos/2021_02/thumb_files/800_0_1612944601-7571.jpg?r=351097"},
-  {name:"Фруктовый сок", category:"Напитки", price:"3.10 ₼", img:"https://storage.agriexpert.ru/storage/images/articles/3955/model/xl/LCqp2h1UJ9cO.jpg"},
-  {name:"Минеральная вода", category:"Напитки", price:"1.00 ₼", img:"https://static.tildacdn.com/tild3762-3339-4537-b437-366161613838/lechenie-mineralnoj-.jpg"},
-  {name:"Энергетик", category:"Напитки", price:"3.90 ₼", img:"https://s0.rbk.ru/v6_top_pics/media/img/3/37/347222628354373.webp"}
-];
-
 const defaultProducts = productsContainer.innerHTML;
 
 function getCart(){
@@ -43,17 +11,43 @@ function saveCart(cart){
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
+function updateCartCount(){
+
+  const cartBtn = document.querySelector(".cart");
+  const cart = getCart();
+
+  let count = 0;
+
+  cart.forEach(item=>{
+    count += item.count;
+  });
+
+  if(cartBtn){
+    
+    cartBtn.innerHTML = `
+    <img src="icons/cart.png" class="cart-icon">
+    <span>· ${count}</span>`;
+
+  }
+
+}
+
 function activateButtons(){
+
   const buttons = document.querySelectorAll(".add");
 
   buttons.forEach(button => {
+
     button.addEventListener("click", () => {
+
       const product = button.closest(".product");
+
       const name = product.querySelector("h3").textContent;
       const price = product.querySelector(".price").textContent;
       const img = product.querySelector("img").src;
 
       let cart = getCart();
+
       const item = cart.find(p => p.name === name);
 
       if(item){
@@ -64,19 +58,26 @@ function activateButtons(){
 
       saveCart(cart);
 
+      updateCartCount();
+
       button.textContent = "Добавлено";
 
       setTimeout(() => {
         button.textContent = "Добавить";
       }, 500);
+
     });
+
   });
+
 }
 
 function renderProducts(list){
+
   productsContainer.innerHTML = "";
 
   list.forEach(product => {
+
     productsContainer.innerHTML += `
       <div class="product">
         <img src="${product.img}" alt="${product.name}">
@@ -90,15 +91,20 @@ function renderProducts(list){
         </div>
       </div>
     `;
+
   });
 
   activateButtons();
+
 }
 
 activateButtons();
+updateCartCount();
 
 if(search){
+
   search.addEventListener("input", () => {
+
     const value = search.value.toLowerCase().trim();
 
     if(value === ""){
@@ -113,5 +119,23 @@ if(search){
     );
 
     renderProducts(result);
+
   });
+
 }
+
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load",()=>{
+
+if(loader){
+
+setTimeout(()=>{
+
+loader.classList.add("hide");
+
+},1500);
+
+}
+
+});
